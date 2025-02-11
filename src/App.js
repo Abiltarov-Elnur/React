b4687dcfa879b1be7d2aa0cda3fd5e534658d517
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-11 16:53:19 +0300

    page_20

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
 
 function App() {
-	const str1 = 'text1';
-	const str2 = 'text1';
-	const name = 'user';
-	const age  = '30';
-	const arr = [1, 2, 3, 4, 5];
-	const obj = {name: 'john', surname: 'smit'};
-	const attr = 'block';
+	const teg = <><ul>
+		<li>12</li>
+		<li>12</li>
+	</ul></>
 
-	return (<><div>
-		<p>{str1}</p>
-		<p>{str2}</p>
-	</div>
-	<div>
-		name: {name}
-		age: {age}
-	</div>
-	
-	<ul>
-		<li>{arr[0]}</li>
-		<li>{arr[1]}</li>
-		<li>{arr[2]}</li>
-		<li>{arr[3]}</li>
-		<li>{arr[4]}</li>
-	</ul>
+	const items = <><li>text1</li>
+	<li>text2</li><li>text3</li></>
 
-	<p>
-		name:    <span>{obj.name}</span>, <br />
-		surname: <span>{obj.surname}</span>,
-	</p>
-	
-	
-	<div id={attr}>
-		text
-	</div>
-
+	return (
+	<>
+		{teg}
+		{items}
 	</>
 	);
 }
