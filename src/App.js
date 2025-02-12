0446de32c9550bdac1aa7188084870bc08643561
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-12 21:18:20 +0300

    page_27

--- src/App.js
+++ src/App.js
@@ -1,74 +1,16 @@
 import React from 'react';
 
 function App() {
-	const teg = 
-	<>
-		<input /><br />
-		<input /><br />
-		<input /><br />
-	</>
+	let text;
+	const isAdult = 18
 
-	const ul_item = 
-	<>
-		<ul>
-			<li>1</li>
-			<li>1</li>
-			<li>1</li>
-			<li>1</li>
-			<li>1</li>
-		</ul>
-	</>
-
-	const tbl_itm =
-	<>
-		<table>
-			<thead>
-				<tr>
-					<td>
-						12
-					</td>
-					<td>
-						12
-					</td>
-					<td>
-						12
-					</td>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>
-						12
-					</td>
-					<td>
-						12
-					</td>
-					<td>
-						12
-					</td>
-				</tr>
-			</tbody>
-			<tfoot>
-				<tr>
-					<td>
-						12
-					</td><td>
-						12
-					</td><td>
-						12
-					</td>
-				</tr>
-			</tfoot>
-		</table>
-	</>
-
-	return (
-	<>
-		{teg}
-		{ul_item}
-		{tbl_itm}
-	</>
-	);
+	if(isAdult > 17){
+		text = <p>text1</p>
+	}else{
+		text = <p>text2</p>
+	}
+ 
+	return text
 }
 
 export default App;
