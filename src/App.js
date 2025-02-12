98d2d7fc5c0621583fc9645ac5652c8679c0ade1
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-12 21:25:41 +0300

    page_30

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import React from 'react';
 function App() {
 	let text;
 	const isAdult = 18
+	const age = 19;
+	const isAuth = true;
 
 	if(isAdult > 17){
 		text = <p>text1</p>
 	}else{
 		text = <p>text2</p>
 	}
+
+
  
-	return text
+	return (
+	<>
+		<div>
+			{age > 18 ? <p>adult</p> : <p>young</p>}
+		</div>
+
+		<div>
+			{!isAuth && <p>text1</p>}
+		</div>
+	</>
+	);
+
 }
 
 export default App;
