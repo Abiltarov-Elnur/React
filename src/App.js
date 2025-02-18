b4d3f39c5b150aa7339a4bc73d0b6b6d039b4ed9
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-18 12:43:56 +0300

    page_34

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
 function App() {
-	function show1() {
-		alert(1);
+
+	function me(text){
+		alert(text);
 	}
-	
-	function show2() {
-		alert(2);
-	}
-	
+
 	return <div>
-		<button onClick={show1}>act1</button>
-		<button onClick={show2}>act2</button>
+		<button onClick={() => me('number 1')}>act1</button>
+		<button onClick={() => me('number 2')}>act2</button>
+		<button onClick={() => me('number 3')}>act3</button>
 	</div>;
 }
 
