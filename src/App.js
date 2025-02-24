fd85e77d538a42070deddbd6f14b19d379d751d5
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-24 19:02:00 +0300

    page_53

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import uuid from "react-uuid";
 
 function App() {
-	const [name, setName] = useState('name1');
-	const [surn, setSurn] = useState('surn');
-	const [age, setAge] = useState('age');
+	const [name, setName] = useState('Вася');
+	const [surn, setSurn] = useState('Левин');
+	const [age, setAge] = useState('23');
 
 
 	return <div>
 		<span>{name} </span>
 		<span>{surn} </span>
 		<span>{age} </span>
+		<button onClick={() => setName('ИГоооооорь')} and >нажми, тварь!</button>
 	</div>;
 }
 export default App;
