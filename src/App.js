2a83de4318a608c2a0bdd8aaf56b578b9d7e95e0
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-24 19:14:44 +0300

    page_55

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react';
 import uuid from "react-uuid";
 
 function App() {
-	const [ban, setBan] = useState(false);
+	const [count, setCount] = useState(0);
+	
+	function clickHandler() {
+		setCount(count + 1);
+	}
 
-
+	function clickHandler1() {
+		setCount(count - 1);
+	}
+	
 	return <div>
-		<span> {ban ? 'БАН' : 'РАЗБАН'}</span>
-		<button onClick={() => setBan(true)}>нажми, тварь!</button>
-		<button onClick={() => setBan(false)}>нажми, тварь!</button>
+		<span>{count}</span>
+		<button onClick={clickHandler}>+</button>
+		<button onClick={clickHandler1}>-</button>
 	</div>;
-
 }
+
 export default App;
-  
