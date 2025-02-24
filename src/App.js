0b4cbb447e6b28e5627baa2a583dc5dd67480d2f
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-24 18:55:01 +0300

    page_52

--- src/App.js
+++ src/App.js
@@ -2,32 +2,16 @@ import React, { useState } from 'react';
 import uuid from "react-uuid";
 
 function App() {
-	const users = [
-		{id: uuid(), name: 'user1', surn: 'surn1', 
-			age: 30}, 
-		{id: uuid(), name: 'user2', surn: 'surn2', 
-			age: 31}, 
-		{id: uuid(), name: 'user3', surn: 'surn3', 
-			age: 32}, 
-	];
+	const [name, setName] = useState('name1');
+	const [surn, setSurn] = useState('surn');
+	const [age, setAge] = useState('age');
 
-	const iser = users.map(function(item){
-		return <tr key={item.id}>
-			<td>{item.name}</td>
-			<td>{item.surn}</td>
-		</tr>
-	});
 
-	return <table>
-		<thead>
-			<tr>
-				<td>header</td>
-			</tr>
-		</thead>
-		<tbody>
-			{iser}
-		</tbody>
-	</table>
+	return <div>
+		<span>{name} </span>
+		<span>{surn} </span>
+		<span>{age} </span>
+	</div>;
 }
-
 export default App;
+  
