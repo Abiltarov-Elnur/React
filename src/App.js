26435892108314cb070d7e93fac75be869ea6998
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-24 08:50:48 +0300

    page_42

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 function App() {
-	const arr = ['a', 'b', 'c', 'd'];
-	const res = arr.map(function(item) { return <li>{item}</li>; });
+	const users = [
+		{id: 1, name: 'user1', surn: 'surn1', age: 
+			30}, 
+		{id: 1, name: 'user2', surn: 'surn2', age: 
+			31}, 
+		{id: 1, name: 'user3', surn: 'surn3', age: 
+			32}, 
+	];
 
-	
+	const res = users.map(function(item, index){
+		return <ul key = {item.id}>
+			<li>{item.name}</li>:
+			<li>{item.surn}</li>:
+			<li>{item.age}</li>
+		</ul>
+	});
+
 	return <ul>
 		{res}
 	</ul>;
