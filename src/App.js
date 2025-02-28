6c881b92b770bb125b699ae68dd6b7c95c06d8e3
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-02-28 11:07:31 +0300

    page_57

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { useState } from 'react';
 import uuid from "react-uuid";
 
-function App() {
-	const [value1, setValue1] = useState('');
-	const [value, setValue] = useState('');
-	
-	function handleChange(event) {
-		setValue(event.target.value);  
-	}
+import React, { useState } from 'react';
 
-	function handleChange1(event) {
-		setValue1(event.target.value);  
-	}
-	
-	return <div>
-		<input value={value} onChange={handleChange} 
-			/> 
-		<input value={value1} onChange={handleChange1} 
-			/> 
-		<p>text: {value}</p>
-		<p>text: {value1}</p>
-	</div>;
+function App() {
+    const [value, setValue] = useState(''); // Состояние для хранения значения инпута
+
+    function handleChange(event) {
+        setValue(event.target.value); // Обновляем состояние при вводе текста
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={value} // Значение инпута привязано к состоянию
+                onChange={handleChange} // Обработчик изменений
+            />
+            <p>Количество символов: {value.length}</p> {/* Выводим длину введенного текста */}
+        </div>
+    );
 }
 
 export default App;
+
