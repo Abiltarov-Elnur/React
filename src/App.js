a77a481e191eaa6dbb2ed293f1f28cd0aef884ea
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-03 15:33:15 +0300

    Задание 66

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import uuid from "react-uuid";
 import React, { useState } from 'react';
 
 function App() {
-    const texts = ['Mos', 'DZA', 'Simf']; 
-    const [value, setValue] = useState('');
-
-    const options = texts.map((text, index) => {
-        return <option key={index}>{text}</option>;
-    });
-
-    return (
-        <div>
-            <select 
-                value={value} 
-                onChange={(event) => setValue(event.target.value)} 
-            >
-                {options}
-            </select>
-            <p>ваш выбор: {value}</p>
-        </div>
-    );
+	const [value, setValue] = useState('');
+	
+	return <div>
+		<select value={value} 
+		onChange={event => setValue(event.target.value)}> 
+			<option value="1">text1</option>
+			<option value="2">text2</option>
+			<option value="3">text3</option>
+		</select>
+		<p>
+			{value === '1' && 'вы выбрали первый пункт'} 
+			{value === '2' && 'вы выбрали второй пункт'} 
+			{value === '3' && 'вы выбрали третий пункт'} 
+		</p>
+	</div>;
 }
 
 export default App;
