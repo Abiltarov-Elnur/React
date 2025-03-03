f1a465b7b463ff855755e3d82009569c2dae6875
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-03 13:34:01 +0300

    Задание 62

--- src/App.js
+++ src/App.js
@@ -4,37 +4,21 @@ import React, { useState } from 'react';
 
 
 function App() {
-    const [value, setValue] = useState('');
-
-    // Функция для транслитерации текста
-    const transliterate = (text) => {
-        const cyrillicToLatin = {
-            'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo',
-            'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
-            'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
-            'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
-            'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
-            'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo',
-            'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
-            'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
-            'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch',
-            'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
-        };
-
-        // Преобразуем каждый символ текста
-        return text.split('').map(char => cyrillicToLatin[char] || char).join('');
-    };
-
-    return (
-        <div>
-            <textarea 
-                value={value} 
-                onChange={(event) => setValue(event.target.value)} 
-                placeholder="Введите текст на кириллице"
-            />
-            <p>Транслит: {transliterate(value)}</p>
-        </div>
-    );
+	const [checked, setChecked] = useState(true);
+    const [checked1, setChecked1] = useState(true);
+    const [checked2, setChecked2] = useState(true);
+	
+	return <div>
+        <p>HTML:</p>
+		<input type="checkbox" checked={checked} 
+			onChange={() => setChecked(!checked)} /> 
+        <p>CSS:</p>
+		<input type="checkbox" checked1={checked} 
+			onChange={() => setChecked1(!checked)} /> 
+        <p>JS:</p>
+		<input type="checkbox" checked2={checked} 
+			onChange={() => setChecked2(!checked)} /> 
+	</div>;
 }
 
 export default App;
