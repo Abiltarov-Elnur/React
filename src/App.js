0596cf7417f0bb58602c1f44ecf2cf8941dd68cc
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-03 15:30:15 +0300

    Задание 65

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import uuid from "react-uuid";
 
 import React, { useState } from 'react';
 
-
 function App() {
-	const [value, setValue] = useState('');
-	
-	function handleChange(event) {
-		setValue(event.target.value);
-	}
-	
-	return <div>
-		<select value={value} onChange={handleChange}>
-			<option>Moscow</option>
-			<option>Dzankoy</option>
-			<option>Simf</option>
-			<option>Zarechnoyes</option>
-		</select>
-		<p>
-			ваш выбор: {value}
-		</p>
-	</div>;
+    const texts = ['Mos', 'DZA', 'Simf']; 
+    const [value, setValue] = useState('');
+
+    const options = texts.map((text, index) => {
+        return <option key={index}>{text}</option>;
+    });
+
+    return (
+        <div>
+            <select 
+                value={value} 
+                onChange={(event) => setValue(event.target.value)} 
+            >
+                {options}
+            </select>
+            <p>ваш выбор: {value}</p>
+        </div>
+    );
 }
+
 export default App;
 
