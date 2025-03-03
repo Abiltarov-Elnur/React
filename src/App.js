4cec307a5c2d36456ee310040962401c133f87cc
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-03 13:15:52 +0300

    Задание 59

--- src/App.js
+++ src/App.js
@@ -4,23 +4,58 @@ import React, { useState } from 'react';
 
 
 function App() {
-    const [age, setAge] = useState(0);
+    const [val1, setNum1] = useState("");
+    const [val2, setNum2] = useState("");
+    const [val3, setNum3] = useState("");
+    const [val4, setNum4] = useState("");
+    const [val5, setNum5] = useState("");
 
-    function handleChange(event) {
-        setAge(event.target.value);
+    function handleChange1(event) {
+        setNum1(+event.target.value);
+    }
+    function handleChange2(event) {
+        setNum2(+event.target.value);
+    }
+    function handleChange3(event) {
+        setNum3(+event.target.value);
+    }
+    function handleChange4(event) {
+        setNum4(+event.target.value);
+    }
+    function handleChange5(event) {
+        setNum5(+event.target.value);
     }
 
     // Получаем текущий год
-    var grs = (age * (9/5)) + 32
-
+    
     return (
         <div>
             <input 
                 type="number" 
-                value={age} 
-                onChange={handleChange} 
+                value={val1} 
+                onChange={handleChange1} 
             />
-            <p>Градус: {grs}</p>
+            <input 
+                type="number" 
+                value={val2} 
+                onChange={handleChange2} 
+            />
+            <input 
+                type="number" 
+                value={val3} 
+                onChange={handleChange3} 
+            />
+            <input 
+                type="number" 
+                value={val4} 
+                onChange={handleChange4} 
+            />
+            <input 
+                type="number" 
+                value={val5} 
+                onChange={handleChange5} 
+            />
+            <p>Res: {(val1+val2+val3+val4+val5)/5}</p>
         </div>
     );
 }
