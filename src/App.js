339f5675f3baed758504dd3c38cf238425f25de9
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-03 15:18:38 +0300

    Задание 63

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ function App() {
     const [checked1, setChecked1] = useState(true);
     const [checked2, setChecked2] = useState(true);
 	
+
+	let mes;
+	if(checked){
+		mes = <p>Ура вам есть 18!</p>
+
+	}else{
+		mes = <p>Пошел вон!</p>
+	}
+
+
 	return <div>
-        <p>HTML:</p>
+        <p>Тебе есть 18?</p>
 		<input type="checkbox" checked={checked} 
 			onChange={() => setChecked(!checked)} /> 
-        <p>CSS:</p>
-		<input type="checkbox" checked1={checked} 
-			onChange={() => setChecked1(!checked)} /> 
-        <p>JS:</p>
-		<input type="checkbox" checked2={checked} 
-			onChange={() => setChecked2(!checked)} /> 
+        <p>{mes}</p>
 	</div>;
 }
 
