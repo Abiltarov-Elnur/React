8f738b3ceffc78fe4989d4d638cd3334244558e0
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-04 12:51:26 +0300

    Задание 77

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import uuid from "react-uuid";
-
 import React, { useState } from "react";
 
 function App() {
-	const [notes, setNotes] = useState([1, 
-		2, 3, 4, 5]); 
-	
+  const initNotes = [
+    {
+      id: uuid(),
+      name: "name1",
+      desc: "long description 1",
+      show: false, 
+    },
+    {
+      id: uuid(),
+      name: "name2",
+      desc: "long description 2",
+      show: false,
+    },
+    {
+      id: uuid(),
+      name: "name3",
+      desc: "long description 3",
+      show: false,
+    },
+  ];
 
-	function doSmth(index) {
-		let copy = [...notes];
-		copy.splice(index, 1); 
-		setNotes(copy);
-	}
+  const [notes, setNotes] = useState(initNotes);
 
+  function toggleDescription(id) {
+    setNotes(
+      notes.map((note) =>
+        note.id === id ? { ...note, show: !note.show } : note
+      )
+    );
+  }
 
-	const result = notes.map((note, index) => {
-		return <li key={index} onClick={() => doSmth(index)}> 
-			{note}
-		</li>;
-	});
-	
-	return <div>
-		<ul>
-			{result}
-		</ul>
-	</div>;
+  const result = notes.map((note) => (
+    <p key={note.id}>
+      {note.name}
+      {note.show && <i> — {note.desc}</i>}
+      <button onClick={() => toggleDescription(note.id)}>
+        {note.show ? "Скрыть" : "Показать"}
+      </button>
+    </p>
+  ));
+
+  return <div>{result}</div>;
 }
 
 export default App;
