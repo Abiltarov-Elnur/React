0d227bee60983cbe131ccdac2e6dcdfab0a92fb5
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-05 11:57:06 +0300

    Задание 79

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
-import uuid from "react-uuid";
-import React, { useState } from "react";
+import React from 'react';
+import Product from './Product'; 
 
 function App() {
-  const initNotes = [
-    {
-      id: uuid(),
-      name: "name1",
-      desc: "long description 1",
-      show: false, 
-    },
-    {
-      id: uuid(),
-      name: "name2",
-      desc: "long description 2",
-      show: false,
-    },
-    {
-      id: uuid(),
-      name: "name3",
-      desc: "long description 3",
-      show: false,
-    },
-  ];
-
-  const [notes, setNotes] = useState(initNotes);
-
-  function toggleDescription(id) {
-    setNotes(
-      notes.map((note) =>
-        note.id === id ? { ...note, show: !note.show } : note
-      )
-    );
-  }
-
-  const result = notes.map((note) => (
-    <p key={note.id}>
-      {note.name}
-      {note.show && <i> — {note.desc}</i>}
-      <button onClick={() => toggleDescription(note.id)}>
-        {note.show ? "Скрыть" : "Показать"}
-      </button>
-    </p>
-  ));
-
-  return <div>{result}</div>;
+	return <div>
+		<Product />
+	</div>;
 }
 
 export default App;
---
+++ src/Product.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Product() {
+	return <div>
+        <p>
+		    Elnur
+	    </p>
+        <p>
+            Abiltarov
+        </p>
+        <p>
+            Serverovich
+        </p>
+        
+    </div>
+}
+
+export default Product;
