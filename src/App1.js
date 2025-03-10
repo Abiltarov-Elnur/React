9ac19600deba389c714ff4748c01d0a6c79b0b2c
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-03-10 12:00:34 +0300

    Задание 103

--- src/App1.js
+++ src/App1.js
@@ -3,49 +3,39 @@ import TempInp from "./TempInp";
 import Verdict from "./Verdict"; 
 //import './style.css';
 import { styles } from "./styles";		
+import styled from 'styled-components';
 
+
+const Container = styled.div`
+	width: 200px;
+	border: 2px solid brown;
+	padding: 10px;
+	text-align: center;
+`;
+
+const Text1 = styled.p`
+	color: orangered;
+	font-weight: bold;
+`;
+
+const Text2 = styled.p`
+	font-style: Bold;
+	color: brown;
+`;
+
+const Text3 = styled.p`
+	background-color: orange;
+	font-weight: bold;
+	color: white;
+`;
 
 function App() {
-
-	//для div:
-	const wd1 = '200px';
-	const br1 = '2px solid brown';
-	const pd1 = '10px';
-	const ta1 = 'center';
-
-	//для первого p:
-	const co1 = 'orangered';
-	const fw1 = 'italic';
-
-	//для второго p:
-	const fs1 = 'italic';
-	const co2 = 'brown';
-	
-	//для третьего p:
-	const bco1 = 'orange';
-	const co3 = 'white';
-
 	return (
-		<div style = {{
-			width: wd1,
-			border: br1,
-			padding: pd1,
-			textAlign: ta1 }}>
-			<p style = {{ color: co1, fontWeight: 
-				fw1 }}> 
-				text
-			</p>
-			<p style = {{ fontStyle: fs1, color: 
-				co2 }}> 
-				text
-			</p>
-			<p style = {{ 
-				backgroundColor: bco1,
-				fontWeight: fw1,
-				color: co3 }}>
-				text
-			</p>
-		</div>
+		<Container>
+			<Text1>text</Text1>
+			<Text2>text</Text2>
+			<Text3>text</Text3>
+		</Container>
 	);
 }
 
